9266b6f5aaca7a1bb27667da74b87a3a08611b8b
andysaktia <andysaktia@gmail.com>
2022-08-18 18:58:31 +0700

    add script + script merge

---
+++ Day03-Data_Structure_Java/Day3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day3 {
+    public static void main(String[] args) {
+
+        // Scanner scanner = new Scanner(System.in);
+
+        /*
+         * String text = scanner.nextLine(); int num = scanner.nextInt();
+         * 
+         */
+
+        // 1 array merge
+        // Input: ['kazuya', 'jin', 'lee'], ['kazuya', 'feng']
+        // Output: ['kazuya', 'jin', 'lee', 'feng']
+
+        ArrayList<String> listOne = new ArrayList<>(Arrays.asList("kazuya", "jin", "lee"));
+        ArrayList<String> listTwo = new ArrayList<>(Arrays.asList("kazuya", "feng"));
+
+        List<String> listTwoCopy = new ArrayList<>(listTwo);
+        listTwoCopy.removeAll(listOne);
+        listOne.addAll(listTwoCopy);
+
+        System.out.println(listOne);
+
+    }
+}
